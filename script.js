6955e1992e9f84d0dd02a0779f2749ea4ad11c04
hrisso <heather.risso@gmail.com>
2020-12-22 14:08:43 -0500

    search results almost work

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const exerciseImageUrl = "https://wger.de/api/v2/exerciseimage/?is_main=True"
+
 const muscleUrl = "https://wger.de/api/v2/muscle"
 
 
@@ -23,7 +23,11 @@ function muscleOptions(muscleList) {
   return muscleList.forEach((muscle) => {
     const option = document.createElement('option')
     // console.log(muscle.name)
-    option.value = `${muscle.id}`
+    let whole_string= muscle.image_url_main
+    let split_string = whole_string.split(/(\d+)/)
+    let id = split_string[1]
+    // console.log(id)
+    option.value = `${id}`
     option.textContent = `${muscle.name}`
     select.append(option)
   })
@@ -39,29 +43,50 @@ form.addEventListener('submit', getSelection)
 function getSelection(e) {
   e.preventDefault()
   const option = document.querySelector('#select-muscle').value
-  console.log(option)
-  // getExercises(option)
+  // console.log(option)
+  getExercises(option)
 }
 
 
 // API call with dropdown value to retrieve matching exercises & info
-// API call for matching exercise images
 const getExercises = async (id) => {
   const exerciseUrl = `https://wger.de/api/v2/exercise/?muscle=${id}&language=2`
   try {
     const response = await axios.get(exerciseUrl)
     console.log(response.data.results)
-    // const exerciseList = response.data.results
-    // console.log(exerciseList)
+    const exerciseList = response.data.results
+    showExerciseInfo(exerciseList)
   } catch (error) {
     console.log(error)
   }
 }
 
-// getExercises()
+
+// API call for matching exercise images
+async function getImage(id) {
+  const exerciseImageUrl = `https://wger.de/api/v2/exerciseimage/?is_main=True&exercise=${id}`
+  try {
+    const response = await axios.get(exerciseImageUrl)
+    console.log(response.data.results)
+    const exerciseImage = response.data.results
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 // Create tags dynamically & append to append exercises div
-
+function showExerciseInfo(data) {
+  data.forEach((exercise) => {
+    console.log(data.id)
+    const exerciseInfo = `
+    <h3>${exercise.name}</h3>
+    <p>${exercise.description}</p>
+    `
+    // <img src="${exerciseImage}" alt= "exercise" class="exercise"/>
+    document.querySelector('#append-exercises').insertAdjacentHTML('beforeend', exerciseInfo)
+    })
+  }
+    
 
 
 
